974f8947a5f2394bbe46f53e699227e8a95baaa2
Jose <jose.veliz@rappi.com>
2023-07-05 18:51:07 -0400

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
+  site: 'https://duccem.github.io',
+  base: '/personal-page',
 });
